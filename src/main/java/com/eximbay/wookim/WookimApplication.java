d66980ec2702963bb4631bb3cb27f024dc527411
wookim789 <wookim789@gmail.com>
2021-05-21 22:43:47 +0900

    프로젝트 빌드

---
+++ src/main/java/com/eximbay/wookim/WookimApplication.java
@@ -0,0 +1,13 @@
+package com.eximbay.wookim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WookimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WookimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eximbay/wookim/WookimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eximbay.wookim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WookimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
