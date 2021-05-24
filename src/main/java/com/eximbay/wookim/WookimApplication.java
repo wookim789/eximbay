afaf8f405c2822408407642e7497a6f32eec27cc
wookim789 <wookim789@gmail.com>
2021-05-25 00:10:32 +0900

    develop eximbay 과제 준비
    - 카프카 설정 준비
    - 몽고디비 준비

--- src/main/java/com/eximbay/wookim/WookimApplication.java
+++ src/main/java/com/eximbay/wookim/WookimApplication.java
@@ -1,15 +1,35 @@
 package com.eximbay.wookim;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
+import java.util.Properties;
 
 @SpringBootApplication
-@EnableScheduling
 public class WookimApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WookimApplication.class, args);
-    }
 
+        Properties config = new Properties();
+        config.put("bootstrap.servers", "localhost:9092");
+        config.put("group.id", "sales_group");
+        config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        config.put("values.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(config);
+
+        consumer.subscribe(Arrays.asList("sales"));
+
+        while(true){
+            ConsumerRecords<String, String> records = consumer.poll(500);
+            for(ConsumerRecord<String, String> record: records){
+                System.out.println(record.value());
+            }
+        }
+    }
 }
