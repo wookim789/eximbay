cdf1baf12eb314ed81ad65c5b4fb95f8b83c901c
wookim789 <wookim789@gmail.com>
2021-05-28 23:02:29 +0900

    develop eximbay 데이터 저장응ㄹ 위한 작업
    - 카프카 데이터 json 처리
    - mongo db 저장 로직 필요

--- src/main/java/com/eximbay/wookim/WookimApplication.java
+++ src/main/java/com/eximbay/wookim/WookimApplication.java
@@ -1,21 +1,33 @@
 package com.eximbay.wookim;
 
 import com.eximbay.wookim.document.SalesDoc;
+import com.eximbay.wookim.repository.SalesRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class WookimApplication {
 
-    public static void main(String[] args) {
+    @Autowired
+    private static SalesRepo repo;
+    public static void main(String[] args) throws Exception{
         SpringApplication.run(WookimApplication.class, args);
+
+        ObjectMapper mapper = new ObjectMapper();
 
         Properties config = new Properties();
         config.put("bootstrap.servers", "localhost:9092");
@@ -27,32 +39,37 @@ public class WookimApplication {
 
         consumer.subscribe(Arrays.asList("sales"));
 
-        ObjectMapper mapper = new ObjectMapper();
         while(true){
             ConsumerRecords<String, String> records = consumer.poll(500);
             for(ConsumerRecord<String, String> record: records){
-//                System.out.println(record.value());
+                System.out.println(record.value());
                 try{
                     SalesDoc doc = mapper.readValue(record.value(), SalesDoc.class);
+                    repo.save(doc);
                 }catch (Exception e){
-
+                    e.printStackTrace();
                 }
             }
         }
+//        printJson(mapper);
     }
 
-    private void printJson(){
-//        {
-//            "id": 1,
-//                "title": "title1",
-//                "interfaceType": "api",
-//                "price": "1000",
-//                "regDate": "",
-//                "modDate": ""
-//        }
-        Integer id = 1;
-        String title = "title" + id;
-        String interfaceType = "api";
+    private static void printJson(ObjectMapper mapper) throws Exception{
+        for(int id = 0; id < 1000; id++){
+            String title = "title" + id;
+            String interfaceType = "api";
+            String price = Integer.toString(1000 + id * 5);
+            String regDate = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE);
+
+            Map<String, Object> map = new HashMap<>();
+
+            map.put("id", id);
+            map.put("title", title);
+            map.put("interfaceType", interfaceType);
+            map.put("price", price);
+            map.put("regDate", regDate);
+            System.out.println(mapper.writeValueAsString(map));
+        }
 
     }
 }
